318c01b68b4ad724db9668907927bd7b5ac08fec
matheuscarv69 <matheus9126@gmail.com>
2021-09-14 23:43:13 -0400

    :tada: first commit

---
+++ src/main/java/backvoteschallenge/BackVotesChallengeApplication.java
@@ -0,0 +1,13 @@
+package backvoteschallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackVotesChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackVotesChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/backvoteschallenge/BackVotesChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package backvoteschallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackVotesChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
